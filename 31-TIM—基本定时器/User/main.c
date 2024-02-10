1f38850a15b3609c6dcf1ca5943cc7af860d2ef7
435191950@qq.com <435191950@qq.com>
2024-02-10 10:26:03 +0800

    TIM-基本定时器

---
+++ 31-TIM—基本定时器/User/main.c
@@ -0,0 +1,47 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2013-xx-xx
+  * @brief   测试led
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 F103-指南者 STM32 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */ 
+	
+#include "stm32f10x.h"
+#include "bsp_led.h"
+#include "bsp_TimeBase.h"
+
+volatile uint32_t time = 0;
+
+/**
+  * @brief  主函数
+  * @param  无  
+  * @retval 无
+  */
+int main(void)
+{	
+	/* LED 端口初始化 */
+	LED_GPIO_Config();	
+
+	BASIC_TIM_Init();	
+
+	while (1)
+	{
+		if(time == 1000)
+		{
+			time = 0;
+			LED1_TOGGLE;
+		}
+	}
+}
+
+
+/*********************************************END OF FILE**********************/
